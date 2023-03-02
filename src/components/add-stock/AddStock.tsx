dcb67a55edb9cf2bd79759598c9d9418c29d29b0
SvetaMura74 <svetlanamortov@gmail.com>
2023-03-02 17:03:44 +0200

    סיום crud

--- src/components/add-stock/AddStock.tsx
+++ src/components/add-stock/AddStock.tsx
@@ -1,7 +1,19 @@
 import { useState } from "react";
 import Modal from "react-modal";
 
+import { useAppDispatch } from "../../app/hooks";
+import { addStock, Stock } from "../../features/stocks/stocksSlice";
+import { v4 } from "uuid";
+import Swal from "sweetalert2";
+
 const AddStock = () => {
+  const [ticker, setTicker] = useState("");
+  const [name, setName] = useState("");
+  const [currency, setCurrency] = useState("");
+  const [exchange_long, setExchange_long] = useState("");
+  const dispatch = useAppDispatch();
+  
+
   const [isOpen, setOpen] = useState(false);
   Modal.setAppElement("#root");
   const closeModal = () => {
@@ -17,7 +29,69 @@ const AddStock = () => {
         Add Stock
       </button>
       <Modal isOpen={isOpen} onRequestClose={closeModal}>
-        Modal
+        <h2>Add a Stock</h2>
+        <button onClick={closeModal}>Close</button>
+        <hr />
+        
+          <label htmlFor="ticker">Symbol ticker:</label>
+          <input
+            id="ticker"
+            value={ticker}
+            onChange={(e) => {
+              setTicker(e.currentTarget.value);
+            }}
+            placeholder="Symbol ticker"
+          />
+
+          <br />
+          <label htmlFor="name">Company name:</label>
+          <input
+            id="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.currentTarget.value);
+            }}
+            placeholder="Company name"
+          />
+          <br />
+          <label htmlFor="currency">Currency:</label>
+          <input
+            id="currency"
+            value={currency}
+            onChange={(e) => {
+              setCurrency(e.currentTarget.value);
+            }}
+            placeholder="Currency"
+          />
+          <br />
+          <label htmlFor="exchange">Stock Exchange Name:</label>
+          <input
+            id="exchange"
+            value={exchange_long}
+            onChange={(e) => {
+              setExchange_long(e.currentTarget.value);
+            }}
+            placeholder=" Stock Exchange Name"
+          />
+          <br/>
+          <button
+            className="btn btn-success"
+            onClick={() => {
+              const stock: Stock = {
+                id: v4(), 
+                ticker: ticker,
+                name: name,
+                exchange_long: exchange_long,
+                currency: currency,
+              };
+              dispatch(addStock(stock));
+              Swal.fire("success"," ","success")
+              closeModal()
+            }}
+          >
+            Save the changes
+          </button>
+        
       </Modal>
     </>
   );
